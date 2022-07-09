ad14d46df0bee4993cd0830311100fd3b9d49d7c
William H. Grover <wgrover@gmail.com>
2022-07-08 20:16:31 -0700

    working locally on flask

--- app.py
+++ app.py
@@ -1,7 +1,32 @@
-from flask import Flask
+from flask import Flask, render_template
+import requests
+from datetime import datetime,timezone,timedelta
+from zoneinfo import ZoneInfo
 
 app = Flask(__name__)
 
 @app.route("/")
-def hello_world():
-    return "<p>Hello, World!</p>"
+def index():
+    now = datetime.now(timezone.utc)
+    url = "https://api-iwls.dfo-mpo.gc.ca/api/v1/stations/5cebf1de3d0f4a073c4bb954/data?time-series-code=wlp&from="
+    url += now.strftime("%Y-%m-%dT%H%%3A%M%%3A%SZ")
+    url += "&to="
+    future = now + timedelta(days=1)
+    url += future.strftime("%Y-%m-%dT%H%%3A%M%%3A%SZ")
+    response = requests.get(url)
+    times = []
+    heights = []
+    for row in response.json():
+        utc = datetime.strptime(row["eventDate"], "%Y-%m-%dT%H:%M:%SZ").replace(tzinfo=ZoneInfo("UTC"))
+        time = utc.astimezone(ZoneInfo("America/Vancouver"))
+        times.append(time)
+        heights.append(row["value"] * 3.28084)
+    print ("Current height is %0.1f feet." % heights[0])
+    if heights[0] >= 10.0:
+        for time, height in zip(times, heights):
+            if height < 10.0:
+                return render_template('index.html', floating=True, height=round(heights[0], 1), time=datetime.strftime(time, "%-I:%M %p on %A %B %-d %Y"))                
+    else:
+        for time, height in zip(times, heights):
+            if height >= 10.0:
+                return render_template('index.html', floating=False, height=round(heights[0], 1), time=datetime.strftime(time, "%-I:%M %p on %A %B %-d %Y")) 
