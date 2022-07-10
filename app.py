1bf202ea874f3342616d83ca69c0120dad65fcc4
William H. Grover <wgrover@gmail.com>
2022-07-09 22:03:07 -0700

    testing out time offsets

--- app.py
+++ app.py
@@ -7,9 +7,9 @@ app = Flask(__name__)
 
 @app.route("/")
 def index():
-    now = datetime.now(timezone.utc)
+    now = datetime.now(timezone.utc) - timedelta(minutes=20)
     url = "https://api-iwls.dfo-mpo.gc.ca/api/v1/stations/5cebf1de3d0f4a073c4bb954/data?time-series-code=wlp&from="
-    url += now.strftime("%Y-%m-%dT%H%%3A%M%%3A%SZ")
+    url += now.strftime("%Y-%m-%dT%H%%3A%M%%3A%SZ") 
     url += "&to="
     future = now + timedelta(days=1)
     url += future.strftime("%Y-%m-%dT%H%%3A%M%%3A%SZ")
@@ -17,7 +17,9 @@ def index():
     times = []
     heights = []
     for row in response.json():
-        utc = datetime.strptime(row["eventDate"], "%Y-%m-%dT%H:%M:%SZ").replace(tzinfo=ZoneInfo("UTC"))
+        utc = datetime.strptime(row["eventDate"], "%Y-%m-%dT%H:%M:%SZ")
+        utc = utc + timedelta(minutes=20)
+        utc = utc.replace(tzinfo=ZoneInfo("UTC"))
         time = utc.astimezone(ZoneInfo("America/Vancouver"))
         times.append(time)
         heights.append(row["value"] * 3.28084)
